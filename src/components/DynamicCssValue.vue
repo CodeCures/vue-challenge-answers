234a63d575b17b50ec505b4005e6829902d0165f
Courage <couragesblog@gmail.com>
2023-06-14 07:47:13 +0100

    Add DynamicCssValue component

---
+++ src/components/DynamicCssValue.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const theme = ref("red");
+const colors = ["blue", "yellow", "red", "green"]
+setInterval(() => {
+  theme.value = colors[Math.floor(Math.random() * 4)]
+}, 1000)
+
+</script>
+
+<template>
+    <p>hello</p>
+</template>
+
+<style scoped>
+p {
+    color: v-bind(theme);
+}
+</style>
