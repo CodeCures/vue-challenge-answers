56e859d61e7be425993d9347a079e8508c7d29bc
Courage <couragesblog@gmail.com>
2023-06-14 16:13:45 +0100

    Test life cycle hooks

---
+++ src/components/Child.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { onMounted, inject, onUnmounted, onBeforeUnmount } from "vue"
+
+const timer = inject("timer")
+const count = inject("count")
+
+onMounted(() => {
+  timer.value = setInterval(() => {
+    count.value++
+  }, 1000)
+})
+
+onBeforeUnmount(() => window.clearInterval(timer.value))
+
+</script>
+
+<template>
+  <div>
+    <p>
+      Child Component: {{ count }}
+    </p>
+  </div>
+</template>
